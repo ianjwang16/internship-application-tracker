165243e19243412edeb0d8a3693dc7d99147a5cd
IW <ianjwang16@gmail.com>
2026-09-16 23:59:35 -0400

    Fix Railway server binding

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("Connected to MongoDB");
 
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on port ${PORT}`);
         });
     })
     .catch((error) => {
